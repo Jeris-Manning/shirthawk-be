c4320d1f70a5690983cb7abeb5dbc0cc3434d4bd
Jeris Manning <the.goose.moose@gmail.com>
2020-03-15 15:40:13 -0600

    design seed fix

--- databaseOperations/seeds/05-designs.js
+++ databaseOperations/seeds/05-designs.js
@@ -1,22 +1,22 @@
 exports.seed = function(knex) {
   return knex("designs").insert([
     {
-      design_url: "https://uxmasters.org/images/ant_logo.svg",
-      thumbnail_url: "https://uxmasters.org/images/ant_logo.svg",
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
       design_name: "Ant for shirt",
       storeID: 1,
       userID: 1
     },
     {
-      design_url: "https://uxmasters.org/images/ant_logo.svg",
-      thumbnail_url: "https://uxmasters.org/images/ant_logo.svg",
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
       design_name: "Ant for shirt",
       storeID: 1,
       userID: 1
     },
     {
-      design_url: "https://uxmasters.org/images/ant_logo.svg",
-      thumbnail_url: "https://uxmasters.org/images/ant_logo.svg",
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
       design_name: "Ant for shirt",
       storeID: 1,
       userID: 1

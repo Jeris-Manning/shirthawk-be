0c1229429838f26a38067cff9f4773d885041901
Jeris Manning <the.goose.moose@gmail.com>
2020-04-07 00:19:06 -0600

    updated seeds

--- databaseOperations/seeds/05-designs.js
+++ databaseOperations/seeds/05-designs.js
@@ -1,25 +1,3 @@
 exports.seed = function(knex) {
-  return knex("designs").insert([
-    {
-      design_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      thumbnail_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      design_name: "Ant for shirt",
-      storeID: 1,
-      userID: 1
-    },
-    {
-      design_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      thumbnail_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      design_name: "Ant for shirt",
-      storeID: 1,
-      userID: 1
-    },
-    {
-      design_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      thumbnail_url: "https://www.redditstatic.com/avatars/avatar_default_18_46A508.png",
-      design_name: "Ant for shirt",
-      storeID: 1,
-      userID: 1
-    }
-  ]);
+    return knex("designs").insert([]);
 };
--- databaseOperations/seeds/06-products.js
+++ databaseOperations/seeds/06-products.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
     {
       productName: "Test Product",
       fullSizeURL:
-        "https://res.cloudinary.com/dze74ofbf/image/upload/v1581453585/wsjikfpellbybgzbymy2.jpg",
+        "https://res.cloudinary.com/ddgiykavr/image/upload/v1585783488/regular_kl0car.jpg",
       thumbnailURL:
-        "https://res.cloudinary.com/dze74ofbf/image/upload/v1581454280/eiz7lg8c8mtosndddelk.jpg",
+        "https://res.cloudinary.com/ddgiykavr/image/upload/v1585783488/regular_kl0car.jpg",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare arcu vulputate arcu suscipit venenatis. Donec sit amet ipsum ac urna dignissim euismod a euismod nisi. Nullam pulvinar odio semper.",
       price: 10.3,

6fe926b8d97bd3d74e0a4a3fc4e1bc6f7ed3643f
Jeris Manning <49968389+Jeris-Manning@users.noreply.github.com>
2021-03-08 15:27:00 -0700

    engine update

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,43 @@
 export const development = {
-  client: 'pg',
-  connection: 'postgres://localhost:5432/',
-  migrations: {
-    directory: "./databaseOperations/migrations"
-  },
-  seeds: {
-    directory: "./databaseOperations/seeds"
-  },
-  useNullAsDefault: true
+    client: 'pg',
+    connection: 'postgres://localhost:5432/',
+    migrations: {
+        directory: './databaseOperations/migrations',
+    },
+    seeds: {
+        directory: './databaseOperations/seeds',
+    },
+    useNullAsDefault: true,
 };
 export const testing = {
-  client: "pg",
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  useNullAsDefault: true,
-  migrations: {
-    directory: "./databaseOperations/migrations"
-  },
-  seeds: {
-    directory: "./databaseOperations/seeds"
-  }
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: './databaseOperations/migrations',
+    },
+    seeds: {
+        directory: './databaseOperations/seeds',
+    },
 };
 export const production = {
-  client: "pg",
-  connection: process.env.DATABASE_URL,
-  ssl: true,
-  pool: {
-    min: 2,
-    max: 10
-  },
-  useNullAsDefault: true,
-  migrations: {
-    directory: "./databaseOperations/migrations"
-  },
-  seeds: {
-    directory: "./databaseOperations/seeds"
-  }
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    ssl: true,
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    useNullAsDefault: true,
+    migrations: {
+        directory: './databaseOperations/migrations',
+    },
+    seeds: {
+        directory: './databaseOperations/seeds',
+    },
+    useNullAsDefault: true,
 };

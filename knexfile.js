4ccb54d64195e2944b067da7dfcb4880817bfe6d
Jeris Manning <49968389+Jeris-Manning@users.noreply.github.com>
2021-03-08 14:51:03 -0700

    .

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,44 @@
-module.exports = {
-    development: {
-        client: 'pg',
-        connection: 'postgres://localhost:5432/',
-        migrations: {
-            directory: "./databaseOperations/migrations"
-          },
-          seeds: {
-            directory: "./databaseOperations/seeds"
-          },
-        useNullAsDefault: true
-      },
-  ////////////////////////////////////////////////////////
-
-  testing: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./databaseOperations/migrations"
-    },
-    seeds: {
-      directory: "./databaseOperations/seeds"
-    }
+export const development = {
+  client: 'pg',
+  connection: 'postgres://localhost:5432/',
+  migrations: {
+    directory: "./databaseOperations/migrations"
   },
-  ////////////////////////////////////////////////////////
-
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./databaseOperations/migrations"
-    },
-    seeds: {
-      directory: "./databaseOperations/seeds"
-    }
+  seeds: {
+    directory: "./databaseOperations/seeds"
+  },
+  useNullAsDefault: true
+};
+export const testing = {
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./databaseOperations/migrations"
+  },
+  seeds: {
+    directory: "./databaseOperations/seeds"
+  }
+};
+export const production = {
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  useNullAsDefault: true,
+  migrations: {
+    directory: "./databaseOperations/migrations"
+  },
+  seeds: {
+    directory: "./databaseOperations/seeds"
   }
 };

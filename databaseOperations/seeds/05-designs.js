ec023c8d2472b0c9792829c9582cb5742abb46f7
Jeris Manning <49968389+Jeris-Manning@users.noreply.github.com>
2021-03-11 10:53:55 -0700

    adds seeds

--- databaseOperations/seeds/05-designs.js
+++ databaseOperations/seeds/05-designs.js
@@ -1,3 +1,25 @@
 exports.seed = function(knex) {
-    return knex("designs").insert([]);
+  return knex("designs").insert([
+    {
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      design_name: "Ant for shirt",
+      storeID: 1,
+      userID: 1
+    },
+    {
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      design_name: "Ant for shirt",
+      storeID: 1,
+      userID: 1
+    },
+    {
+      design_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      thumbnail_url: "http://clipart-library.com/images/rcnr55d9i.png",
+      design_name: "Ant for shirt",
+      storeID: 1,
+      userID: 1
+    }
+  ]);
 };
